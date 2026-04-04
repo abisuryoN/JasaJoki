84ce5cd50df4ac84e30d44de8a1123003601a37c
abisuryoN <suryonegoro2006@gmail.com>
2026-04-04 16:44:46 +0700

    fe

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://api-dualcode.infinityfreeapp.com/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://jasajoki-production-ef3b.up.railway.app/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
